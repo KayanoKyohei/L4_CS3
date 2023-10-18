0d898e576921c9fb967cdf80acaf8642702edee5
Kayano Kyohei <2111140015s@ed.fuk.kindai.ac.jp>
2023-10-18 08:28:30 +0000

    first commit

--- app/controllers/concerns/top_controller.rb
+++ app/controllers/concerns/top_controller.rb
@@ -8,10 +8,17 @@ class TopController < ApplicationController
   end
 
   def login
-    if params[:uid] == 'kindai' and params[:pass] = 'sanriko'
+    logger.debug params[:uid]
+    if User.find_by(uid: params[:uid]) and User.find_by(pass: params[:pass])
       session[:login_uid] = params[:uid]
       redirect_to top_main_path
+    else
+      render　"errorです"
     end
+  end
+  def logout
+    session.delete[:login_uid]
+    redirect_to top_main_path
   end
 end
 
